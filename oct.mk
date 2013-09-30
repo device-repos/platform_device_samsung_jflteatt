607d435db6e97dbb1be0f96f9788b11c9180fbbd
treken <chupekevin@gmail.com>
2013-09-30 08:06:43 -0500

    Create oct.mk

---
+++ oct.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, device/samsung/jflteatt/full_jflteatt.mk)
+
+# Inherit some common OCT stuff.
+$(call inherit-product, vendor/oct/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
+
+# Inherit some common OCT stuff.
+$(call inherit-product, vendor/oct/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteatt TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/oct_jflteatt/jflteatt:4.3/JLS36G/user/release-keys" PRIVATE_BUILD_DESC="jflteatt-user 4.3 JLS36G release-keys"
+
+PRODUCT_DEVICE := jflteatt
+PRODUCT_NAME := oct_jflteatt
